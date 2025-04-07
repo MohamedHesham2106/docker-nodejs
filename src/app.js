5cf24e3af48d958fa3fa404a581f151848c1db34
MohamedHesham2106 <mohamedhesham21062000@gmail.com>
2025-04-07 11:56:52 +0200

    Test workflow

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js Lab 2');
+  res.send('Hello from Docker Lab 2');
 });
 
 app.listen(PORT, () => {
